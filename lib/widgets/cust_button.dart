9cbb8a390bc9a6797da164c9825839f80494beec
braul <braul@BRAULIO_ROG.local>
2023-04-20 16:20:24 -0600

    Actualizar cust_button.dart, .gitignore y 6 más archivos...

--- lib/widgets/cust_button.dart
+++ lib/widgets/cust_button.dart
@@ -3,17 +3,56 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class CustButton extends StatelessWidget {
-  CustButton({super.key, required this.text,required this.onPress});
+  CustButton({super.key, required this.text,required this.onPress, this.icon});
   String text;
   Function() onPress;
+  IconData? icon;
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextButton(
-        child: Text(text),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+             Colors.white,
+            Color.fromARGB(255, 195, 194, 194)
+          ],
+          begin: Alignment.topCenter,end: Alignment.bottomCenter, stops: [.95,1]
+        ),
+        /*boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 20,
+            spreadRadius: 3,
+            offset: Offset(3,3)
+          ),
+                   BoxShadow(
+            color: Colors.black,
+            blurRadius: 20,
+            spreadRadius: 3,
+            offset: Offset(-3,-3)
+          )
+        ],*/
+        borderRadius: BorderRadius.circular(25)
+      ),
+      child: RawMaterialButton(
+        elevation: 8,
+        highlightElevation: 2,
+        hoverColor: Colors.grey.withOpacity(.5),
+        splashColor: Colors.black,
+        child: Container(alignment: Alignment.center, height: 50, width: MediaQuery.of(context).size.width * 0.6, child: Row( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Icon(icon, color: Colors.blue,size: 35,),
+            Text(text),
+            
+          ],
+        )), 
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
         onPressed: () {
-          onPress(),
+          onPress();
         },
+
       ),
     );
   }
